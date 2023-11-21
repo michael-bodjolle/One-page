496a82351ebb1c7f507771898dd95ad8b7bd0e5f
Michael <michael.bodjolle@laplateforme.io>
2023-11-21 23:01:27 +0100

    addComponent + css

---
+++ justone/src/Components/Heading.jsx
@@ -0,0 +1,12 @@
+import Sound from "../assets/sound.png"
+function Heading(){
+    return(
+        <div className="heading">
+           
+                <h1>Sound of future. <br />
+                <img src={Sound} className="sound"/> AI-Powered<br />
+            musique generator</h1>
+        </div>
+    )
+}
+export default Heading
---
+++ justone/src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+function Navbar() {
+
+    return  ( <nav>
+      <h1 className="bigtitle">musai</h1>
+       <ul>
+         <li>
+             <a href=""> About </a>
+         </li>
+         <li>
+         <a href=""> Services </a>
+         </li>
+         <li>
+         <a href=""> Plans</a>
+         </li>
+
+         <li>
+         <a href=""> Contacts</a>
+         </li>
+       </ul>
+
+       <button>GET STARTED</button>
+     </nav>
+     )
+   
+
+}
+export default Navbar
